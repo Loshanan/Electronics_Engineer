8f6c5413d32b65f1974c3f5aa9cf1b02ba0cb0be
Loshanan <loshanan221199@gmail.com>
2025-01-20 16:35:01 +0530

    Ripple carry Adder completed

--- ra_adder/ra_adder.vhd
+++ ra_adder/ra_adder.vhd
@@ -1,21 +1,23 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity ra_adder is
     generic (
-        g_WIDTH : integer := 4
+        g_WIDTH : natural := 4
     );
     port (
         i_term1 : in  std_logic_vector (g_WIDTH - 1 downto 0);      
         i_term2 : in  std_logic_vector (g_WIDTH - 1 downto 0);      
         
-        -- o_cout  : out std_logic;
-        o_sum   : out std_logic_vector (g_WIDTH downto 0)   -- this includes the carry bit
+        o_total   : out std_logic_vector (g_WIDTH downto 0)   -- this includes the carry bit
     );
 end ra_adder;
 
 architecture rtl of ra_adder is 
-    signal r_carry : std_logic_vector (g_WIDTH downto 0);
+    signal w_carry : std_logic_vector (g_WIDTH downto 0);
+    signal w_SUM   : std_logic_vector (g_WIDTH - 1 downto 0);
+    
 
     component full_adder is
         port(
@@ -30,18 +32,18 @@ architecture rtl of ra_adder is
 
 begin
 
-    r_carry(0) <= '0';
-    SET_WIDTH : for ii in 0 to g_WIDTH - 1 generate
-        full_adder_ii : full_adder 
+    w_carry(0) <= '0';
+    ADDERS : for ii in 0 to g_WIDTH - 1 generate
+        full_adder_inst : full_adder 
         port map (
             i_bit1 => i_term1(ii),
             i_bit2 => i_term2(ii),
-            i_cin  => r_carry(ii),
-            o_cout => r_carry(ii + 1),
-            o_sum  => o_sum(ii)  
+            i_cin  => w_carry(ii),
+            o_cout => w_carry(ii + 1),
+            o_sum  => w_SUM(ii)  
         );
-    end generate SET_WIDTH;
+    end generate ADDERS;
 
-    o_sum(g_WIDTH) <= r_carry(g_WIDTH);
+    o_total <= w_carry(g_WIDTH) & w_SUM;
 
 end rtl;
--- ra_adder/ra_adder_tb.vhd
+++ ra_adder/ra_adder_tb.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity ra_adder_tb is
 end entity;
@@ -7,21 +8,21 @@ end entity;
 architecture behave of ra_adder_tb is
     constant c_WIDTH : integer := 4;
     
-    signal r_TERM1 : std_logic_vector (c_WIDTH - 1 downto 0);
-    signal r_TERM2 : std_logic_vector (c_WIDTH - 1 downto 0);
-    signal w_SUM   : std_logic_vector (c_WIDTH downto 0);
+    signal r_ADD1  : std_logic_vector (c_WIDTH - 1 downto 0) := (others => '0');
+    signal r_ADD2  : std_logic_vector (c_WIDTH - 1 downto 0) := (others => '0');
+    signal w_RESULT: std_logic_vector (c_WIDTH downto 0);
 
     component ra_adder is
         generic (
-            g_WIDTH : integer
+            g_WIDTH : natural
         );
         port (
             i_term1 : in  std_logic_vector (g_WIDTH - 1 downto 0);      
             i_term2 : in  std_logic_vector (g_WIDTH - 1 downto 0);      
             
-            o_sum   : out std_logic_vector (g_WIDTH downto 0)
+            o_total : out std_logic_vector (g_WIDTH downto 0)
         );
-    end component;
+    end component ra_adder;
 
 begin
 
@@ -30,18 +31,22 @@ begin
         g_WIDTH => c_WIDTH
     )
     port map (
-        i_term1 => r_TERM1,
-        i_term2 => r_TERM2,
-        o_sum   => w_SUM
+        i_term1 => r_ADD1,
+        i_term2 => r_ADD2,
+        o_total => w_RESULT
     );
 
     process is 
     begin
-        r_TERM1 <= X"2";
-        r_TERM2 <= X"D";
+        r_ADD1 <= "0010";
+        r_ADD2 <= "1101";
         wait for 10 ns;
-        r_TERM1 <= X"3";
-        r_TERM2 <= X"5";
-        wait for 10 ns;        
+        r_ADD1 <= "0011";
+        r_ADD2 <= "1001";
+        wait for 10 ns;  
+        r_ADD1 <= "1111";
+        r_ADD2 <= "1111";
+        wait for 10 ns;  
+        wait;    
     end process;
 end behave;
